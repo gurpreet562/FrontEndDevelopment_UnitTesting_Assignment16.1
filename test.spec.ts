b94a46ae8991e635d4bddfa9b09eaab172e8d991
gurpreet562 <kgurpreet562@gmail.com>
2017-10-30 19:40:42 +0530

    Create test.spec.ts

---
+++ test.spec.ts
@@ -0,0 +1,16 @@
+mport { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls:  ['./banner.component.css']
+})
+export class BannerComponent {
+  title = 'Test Tour of Heroes';
+  beforeEach(async(() => {
+  TestBed.configureTestingModule({
+    declarations: [ BannerComponent ],
+  })
+  .compileComponents(); 
+}));
+}
